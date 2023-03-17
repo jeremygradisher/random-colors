2fec75564d33c1c348ce7eb6214de42d564a69aa
jeremygradisher <jeremygradisher@gmail.com>
2023-03-17 14:43:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Random } from './Random';
+
+export function App() {
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
+}
